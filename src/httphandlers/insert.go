1def22f8801692917e2be4ab51ef4be6f514c705
markelmad <markel.mad@gmail.com>
2026-01-16 22:57:54 +0000

    Fixed: defer on insert

--- src/httphandlers/insert.go
+++ src/httphandlers/insert.go
@@ -7,7 +7,7 @@ import (
 	helper "src/src/helpers"
 )
 
-//http://localhost:9000/insertrow?title=Just&category=Comedy
+// http://localhost:9000/insertrow?title=Just&category=Comedy
 func InsertRow(w http.ResponseWriter, r *http.Request) {
 	query := r.URL.Query()
 	if !query.Has("title") || !query.Has("category") {
@@ -23,10 +23,11 @@ func InsertRow(w http.ResponseWriter, r *http.Request) {
 	category := query["category"][0]
 
 	row, err := db.Query("SELECT title FROM shakespeare_work WHERE title = ?", title)
-	defer row.Close()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	defer row.Close()
+
 	if row.Next() {
 		w.Write([]byte(fmt.Sprintf("Item with a Title of %s already exists! You cannot have duplicate Titles.\n", title)))
 		fmt.Printf("Item with a Title of %s already exists! You cannot have duplicate Titles.\n", title)
@@ -34,10 +35,11 @@ func InsertRow(w http.ResponseWriter, r *http.Request) {
 	}
 
 	stmt, err := db.Prepare("INSERT INTO shakespeare_work (title, category, read_count) VALUES (?, ?, ?)")
-	defer stmt.Close()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	defer stmt.Close()
+
 	_, err1 := stmt.Exec(title, category, 0)
 	if err1 != nil {
 		log.Fatal(err.Error())
